0251088b2aecf954bd58be010566ec6569c9447c
turbopope <PapstDonB@Googlemail.com>
2019-08-03 13:40:43 +0800

    Updates

---
+++ precache-manifest.6f7916959c970242a3008d29f0846abc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2631960f522b9860da174038248eee91",
+    "url": "/bouncing-dvd/index.html"
+  },
+  {
+    "revision": "421b701ff62772132557",
+    "url": "/bouncing-dvd/static/css/main.a835f5ee.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/bouncing-dvd/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "421b701ff62772132557",
+    "url": "/bouncing-dvd/static/js/main.abfe18a5.chunk.js"
+  },
+  {
+    "revision": "91ea88a3d5de148fc39a",
+    "url": "/bouncing-dvd/static/js/runtime~main.e707364a.js"
+  }
+]);
