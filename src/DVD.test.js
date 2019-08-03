097a452bcd6978bc4202acc65ab6ff5082594ad7
turbopope <PapstDonB@Googlemail.com>
2019-08-03 13:54:14 +0800

    Rename App to DVD

--- src/DVD.test.js
+++ src/DVD.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import DVD from './DVD';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<DVD />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
